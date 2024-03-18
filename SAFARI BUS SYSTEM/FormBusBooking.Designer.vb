ceb1c01aeb7dda1700d025128d87313b64d7b569
isferdinand <ikipkogey@kabarak.ac.ke>
2024-03-18 09:14:13 +0300

    Added additinal booking fields

--- SAFARI BUS SYSTEM/FormBusBooking.Designer.vb
+++ SAFARI BUS SYSTEM/FormBusBooking.Designer.vb
@@ -22,85 +22,172 @@ Partial Class FormBooking
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormBooking))
-        ComboBox1 = New ComboBox()
-        ComboBox2 = New ComboBox()
+        ComBoxFrom = New ComboBox()
+        ComBoxTo = New ComboBox()
         DateTimePicker1 = New DateTimePicker()
-        DateTimePicker2 = New DateTimePicker()
-        btnSearch = New Button()
+        ComBoxStation = New ComboBox()
+        BtnBook = New Button()
+        NmrcSeatNo = New NumericUpDown()
+        LblSeatNo = New Label()
+        BtnLayout = New Button()
+        LblHomeTitle = New Label()
+        NmrcPrice = New NumericUpDown()
+        LblPrice = New Label()
+        BtnPrices = New Button()
+        CType(NmrcSeatNo, ComponentModel.ISupportInitialize).BeginInit()
+        CType(NmrcPrice, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
-        ' ComboBox1
+        ' ComBoxFrom
         ' 
-        ComboBox1.FormattingEnabled = True
-        ComboBox1.Items.AddRange(New Object() {"LAMU", "MPEKETONI", "MALINDI", "KILIFI", "MOMBASA", "VOI", "KWALE", "NAIROBI", "NAIVASHA", "NAKURU", "KITALE", "BUNGOMA", "KIAMBU", "BONDO", "KISUMU", "KIRINYAGA", "MERU", "EMBU", "MALABA", "ELDORET", "KAKAMEGA", "NYERI", "TURKANA", "KERICHO", "BOMET", "BUSIA", "KISII", "MACHAKOS", "KITUI", "VIHIGA", "DAR-ES-SALAAM", "ARUSHA", "KAMPALA", "KIGALI"})
-        ComboBox1.Location = New Point(47, 37)
-        ComboBox1.Margin = New Padding(3, 4, 3, 4)
-        ComboBox1.Name = "ComboBox1"
-        ComboBox1.Size = New Size(279, 29)
-        ComboBox1.TabIndex = 0
-        ComboBox1.Text = "From"
+        ComBoxFrom.FormattingEnabled = True
+        ComBoxFrom.Items.AddRange(New Object() {"LAMU", "MPEKETONI", "MALINDI", "KILIFI", "MOMBASA", "VOI", "KWALE", "NAIROBI", "NAIVASHA", "NAKURU", "KITALE", "BUNGOMA", "KIAMBU", "BONDO", "KISUMU", "KIRINYAGA", "MERU", "EMBU", "MALABA", "ELDORET", "KAKAMEGA", "NYERI", "TURKANA", "KERICHO", "BOMET", "BUSIA", "KISII", "MACHAKOS", "KITUI", "VIHIGA", "DAR-ES-SALAAM", "ARUSHA", "KAMPALA", "KIGALI"})
+        ComBoxFrom.Location = New Point(59, 79)
+        ComBoxFrom.Margin = New Padding(3, 4, 3, 4)
+        ComBoxFrom.Name = "ComBoxFrom"
+        ComBoxFrom.Size = New Size(279, 29)
+        ComBoxFrom.TabIndex = 0
+        ComBoxFrom.Text = "From"
         ' 
-        ' ComboBox2
+        ' ComBoxTo
         ' 
-        ComboBox2.FormattingEnabled = True
-        ComboBox2.Items.AddRange(New Object() {"LAMU", "MPEKETONI", "MALINDI", "KILIFI", "MOMBASA", "VOI", "KWALE", "NAIROBI", "NAIVASHA", "NAKURU", "KITALE", "BUNGOMA", "KIAMBU", "BONDO", "KISUMU", "KIRINYAGA", "MERU", "EMBU", "MALABA", "ELDORET", "KAKAMEGA", "NYERI", "TURKANA", "KERICHO", "BOMET", "BUSIA", "KISII", "MACHAKOS", "KITUI", "VIHIGA", "DAR-ES-SALAAM", "ARUSHA", "KAMPALA", "KIGALI"})
-        ComboBox2.Location = New Point(47, 77)
-        ComboBox2.Margin = New Padding(3, 4, 3, 4)
-        ComboBox2.Name = "ComboBox2"
-        ComboBox2.Size = New Size(279, 29)
-        ComboBox2.TabIndex = 2
-        ComboBox2.Text = "To"
+        ComBoxTo.FormattingEnabled = True
+        ComBoxTo.Items.AddRange(New Object() {"LAMU", "MPEKETONI", "MALINDI", "KILIFI", "MOMBASA", "VOI", "KWALE", "NAIROBI", "NAIVASHA", "NAKURU", "KITALE", "BUNGOMA", "KIAMBU", "BONDO", "KISUMU", "KIRINYAGA", "MERU", "EMBU", "MALABA", "ELDORET", "KAKAMEGA", "NYERI", "TURKANA", "KERICHO", "BOMET", "BUSIA", "KISII", "MACHAKOS", "KITUI", "VIHIGA", "DAR-ES-SALAAM", "ARUSHA", "KAMPALA", "KIGALI"})
+        ComBoxTo.Location = New Point(59, 127)
+        ComBoxTo.Margin = New Padding(3, 4, 3, 4)
+        ComBoxTo.Name = "ComBoxTo"
+        ComBoxTo.Size = New Size(279, 29)
+        ComBoxTo.TabIndex = 2
+        ComBoxTo.Text = "To"
         ' 
         ' DateTimePicker1
         ' 
-        DateTimePicker1.Location = New Point(47, 118)
+        DateTimePicker1.Location = New Point(59, 179)
         DateTimePicker1.Margin = New Padding(3, 4, 3, 4)
         DateTimePicker1.Name = "DateTimePicker1"
         DateTimePicker1.Size = New Size(279, 29)
         DateTimePicker1.TabIndex = 3
         ' 
-        ' DateTimePicker2
+        ' ComBoxStation
         ' 
-        DateTimePicker2.Location = New Point(47, 158)
-        DateTimePicker2.Margin = New Padding(3, 4, 3, 4)
-        DateTimePicker2.Name = "DateTimePicker2"
-        DateTimePicker2.Size = New Size(279, 29)
-        DateTimePicker2.TabIndex = 4
+        ComBoxStation.FormattingEnabled = True
+        ComBoxStation.Location = New Point(59, 232)
+        ComBoxStation.Name = "ComBoxStation"
+        ComBoxStation.Size = New Size(279, 29)
+        ComBoxStation.TabIndex = 4
+        ComBoxStation.Text = "Station name"
         ' 
-        ' btnSearch
+        ' BtnBook
         ' 
-        btnSearch.Location = New Point(44, 207)
-        btnSearch.Margin = New Padding(3, 4, 3, 4)
-        btnSearch.Name = "btnSearch"
-        btnSearch.Size = New Size(282, 32)
-        btnSearch.TabIndex = 5
-        btnSearch.Text = "Search"
-        btnSearch.UseVisualStyleBackColor = True
+        BtnBook.Location = New Point(138, 371)
+        BtnBook.Name = "BtnBook"
+        BtnBook.Size = New Size(93, 31)
+        BtnBook.TabIndex = 5
+        BtnBook.Text = "Book"
+        BtnBook.UseVisualStyleBackColor = True
+        ' 
+        ' NmrcSeatNo
+        ' 
+        NmrcSeatNo.Location = New Point(127, 278)
+        NmrcSeatNo.Name = "NmrcSeatNo"
+        NmrcSeatNo.Size = New Size(120, 29)
+        NmrcSeatNo.TabIndex = 7
+        ' 
+        ' LblSeatNo
+        ' 
+        LblSeatNo.AutoSize = True
+        LblSeatNo.BorderStyle = BorderStyle.FixedSingle
+        LblSeatNo.Location = New Point(59, 280)
+        LblSeatNo.Name = "LblSeatNo"
+        LblSeatNo.Size = New Size(64, 23)
+        LblSeatNo.TabIndex = 8
+        LblSeatNo.Text = "Seat No."
+        ' 
+        ' BtnLayout
+        ' 
+        BtnLayout.Location = New Point(253, 276)
+        BtnLayout.Name = "BtnLayout"
+        BtnLayout.Size = New Size(98, 29)
+        BtnLayout.TabIndex = 9
+        BtnLayout.Text = "View Layout"
+        BtnLayout.UseVisualStyleBackColor = True
+        ' 
+        ' LblHomeTitle
+        ' 
+        LblHomeTitle.AutoSize = True
+        LblHomeTitle.Location = New Point(206, 17)
+        LblHomeTitle.Name = "LblHomeTitle"
+        LblHomeTitle.Size = New Size(174, 21)
+        LblHomeTitle.TabIndex = 10
+        LblHomeTitle.Text = "Book Now, Book Cheaper!!"
+        ' 
+        ' NmrcPrice
+        ' 
+        NmrcPrice.Location = New Point(127, 321)
+        NmrcPrice.Maximum = New Decimal(New Integer() {99999, 0, 0, 0})
+        NmrcPrice.Name = "NmrcPrice"
+        NmrcPrice.Size = New Size(120, 29)
+        NmrcPrice.TabIndex = 7
+        ' 
+        ' LblPrice
+        ' 
+        LblPrice.AutoSize = True
+        LblPrice.BorderStyle = BorderStyle.FixedSingle
+        LblPrice.Location = New Point(59, 321)
+        LblPrice.Name = "LblPrice"
+        LblPrice.Size = New Size(41, 23)
+        LblPrice.TabIndex = 8
+        LblPrice.Text = "Price"
+        ' 
+        ' BtnPrices
+        ' 
+        BtnPrices.Location = New Point(253, 321)
+        BtnPrices.Name = "BtnPrices"
+        BtnPrices.Size = New Size(98, 30)
+        BtnPrices.TabIndex = 11
+        BtnPrices.Text = "View Prices"
+        BtnPrices.UseVisualStyleBackColor = True
         ' 
         ' FormBooking
         ' 
         AutoScaleDimensions = New SizeF(8F, 21F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = SystemColors.AppWorkspace
-        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
-        ClientSize = New Size(539, 438)
-        Controls.Add(btnSearch)
-        Controls.Add(DateTimePicker2)
+        ClientSize = New Size(670, 483)
+        Controls.Add(BtnPrices)
+        Controls.Add(LblHomeTitle)
+        Controls.Add(BtnLayout)
+        Controls.Add(LblPrice)
+        Controls.Add(LblSeatNo)
+        Controls.Add(NmrcPrice)
+        Controls.Add(NmrcSeatNo)
+        Controls.Add(BtnBook)
+        Controls.Add(ComBoxStation)
         Controls.Add(DateTimePicker1)
-        Controls.Add(ComboBox2)
-        Controls.Add(ComboBox1)
+        Controls.Add(ComBoxTo)
+        Controls.Add(ComBoxFrom)
         Font = New Font("Segoe Print", 9F, FontStyle.Italic, GraphicsUnit.Point, CByte(0))
         Margin = New Padding(3, 4, 3, 4)
         Name = "FormBooking"
         Text = "SAFARI BUS"
+        CType(NmrcSeatNo, ComponentModel.ISupportInitialize).EndInit()
+        CType(NmrcPrice, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
-    Friend WithEvents ComboBox1 As ComboBox
-    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComBoxFrom As ComboBox
+    Friend WithEvents ComBoxTo As ComboBox
     Friend WithEvents DateTimePicker1 As DateTimePicker
-    Friend WithEvents DateTimePicker2 As DateTimePicker
-    Friend WithEvents btnSearch As Button
+    Friend WithEvents ComBoxStation As ComboBox
+    Friend WithEvents BtnBook As Button
+    Friend WithEvents NmrcSeatNo As NumericUpDown
+    Friend WithEvents LblSeatNo As Label
+    Friend WithEvents BtnLayout As Button
+    Friend WithEvents LblHomeTitle As Label
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents BtnPrices As Button
+    Friend WithEvents NmrcPrice As NumericUpDown
+    Friend WithEvents LblPrice As Label
 
 End Class
--- SAFARI BUS SYSTEM/FormBusBooking.vb
+++ SAFARI BUS SYSTEM/FormBusBooking.vb
@@ -1,5 +1,1 @@
-﻿Public Class FormBooking
-    Private Sub FormBusLayout_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-End Class
+﻿
